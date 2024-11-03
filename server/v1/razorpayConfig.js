41705c2c4667b6651bdfbe2837ba0be34098ec23
LaviSaini <saini.lavi.ls@gmail.com>
2024-11-03 14:41:17 +0530

    changes in razorpayconfig

--- server/v1/razorpayConfig.js
+++ server/v1/razorpayConfig.js
@@ -1,8 +1,8 @@
 const Razorpay = require('razorpay');
 
 const razorpayInstance = new Razorpay({
-    key_id: 'YOUR_RAZORPAY_KEY_ID', 
-    key_secret: 'YOUR_RAZORPAY_SECRET',
+    key_id: 'rzp_test_UvSPbsmSimh3H2', 
+    key_secret: 'AI2dPZ2ySfM4Q4Gr9m01UP9M',
 });
 
 module.exports = razorpayInstance;
