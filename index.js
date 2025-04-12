92a15f2c40a276cac4ce330831bc78464c8fece6
lavi saini <lavi.saini@ebizondigital.com>
2025-04-12 17:28:32 +0530

    changes in cartRoute

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ const DB = dbService("development", config.migrate).start();
 require("./server/connection/connect");
 
 // Middleware: CORS
-app.use(cors({
-    origin: "*",  // Allows all origins
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true
-}));
-
+// app.use(cors({
+//     origin: "*",  // Allows all origins
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//     credentials: true
+// }));
+app.use(cors("*"))
 // Middleware: JSON and URL Encoded Parsing
 app.use(express.json({ limit: "500mb" }));
 app.use(express.urlencoded({ limit: "500mb", extended: false }));
