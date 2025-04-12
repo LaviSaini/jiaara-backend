e328d4926db62d64e3c756b658bdae03ea7e7e94
lavi saini <lavi.saini@ebizondigital.com>
2025-04-12 17:11:09 +0530

    changes in route

--- server/v1/routes/cartRoutes.js
+++ server/v1/routes/cartRoutes.js
@@ -4,6 +4,7 @@ const cartController = require('../controller/cartController');
 const { addItem, deleteItem } = require('../validators/cartValidations');
 
 // Define routes
+
 router.post('/add-cart', addItem, cartController.addItem);    // Add item to cart
 router.get('/get-cart-list/:userId', cartController.getCartList) //get cart items
 router.post('/delete-item', deleteItem, cartController.deleteItem); // Delete item from cart
